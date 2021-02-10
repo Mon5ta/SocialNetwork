5a84cb49505a34d1423bfe2edb20d61c81e8e490
Ilias <iliasavramoglou@gmail.com>
2021-02-10 21:40:23 +0200

    Revert "add initial navbar, add logo"
    This reverts commit a36dbcf86174600f71300f49b024ba7e3b23b8ea.

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php 
-	include 'header.php';
+<?php
+	require 'SQL/connection.php';
 ?>
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SocialNetwork</title>
+</head>
+<body>
+	Hello
 </body>
 </html>
