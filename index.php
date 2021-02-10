a36dbcf86174600f71300f49b024ba7e3b23b8ea
Ilias <iliasavramoglou@gmail.com>
2021-02-10 21:39:33 +0200

    add initial navbar, add logo
    created a very basic navbar with logo of the page and icons (font-awesome) for the menu

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<?php
-	require 'SQL/connection.php';
+<?php 
+	include 'header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>SocialNetwork</title>
-</head>
-<body>
-	Hello
+
+
 </body>
 </html>
